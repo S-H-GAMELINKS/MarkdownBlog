682d68b289757aadb1e20da8fafabafb2300b1d1
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-13 13:21:22 +0900

    using json /api/post/:id (PATCH/PUT)

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -40,14 +40,10 @@ class Api::PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors
     end
   end
 
