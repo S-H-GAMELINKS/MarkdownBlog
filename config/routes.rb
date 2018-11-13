f0c16c1af752234f9a6035dd1834668401702f8f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-13 12:11:40 +0900

    add API for Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'web/index'
   resources :posts
+
+  namespace :api, format: 'json' do
+    resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
