f65f77bc1dad0fd4021dea2e10c549bbc0aa193f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-13 18:13:32 +0900

    add blogs/Index.vue

---
+++ app/javascript/packs/components/blogs/Index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <p v-for="(post, key, index) in posts" :key=index>
+            {{post.title}}
+        </p>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    data: function() {
+        return {
+            posts: []
+        }
+    },
+    mounted: function() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts: function() {
+            axios.get('/api/posts').then((response) => {
+                for(var i = 0; i < response.data.length; i++) {
+                    this.posts.push(response.data[i]);
+                }
+            }, (error) => {
+                console.log(error);
+            })
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
