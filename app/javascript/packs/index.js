a3b961d56e2b6074305acf7bd52e96756e846a69
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-13 18:16:53 +0900

    add router.js

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue/dist/vue.esm';
 import Header from './components/layouts/Header.vue';
+import Router from './router/router';
 
 const app = new Vue({
     el: '#app',
+    router: Router,
     components: {
         'nav-bar': Header
     },
