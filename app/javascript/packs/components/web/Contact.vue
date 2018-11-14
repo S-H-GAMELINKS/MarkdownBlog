6a4c78e2140b2ee999aae405a155d8050400877c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-14 18:00:06 +0900

    add contact

--- app/javascript/packs/components/web/Contact.vue
+++ app/javascript/packs/components/web/Contact.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="container">
         <h1> Contact Pages </h1>
+        <p>mail: gamelinks007@gmail.com</p>
+        <p>github: https://github.com/S-H-GAMELINKS</p>
     </div>
 </template>
