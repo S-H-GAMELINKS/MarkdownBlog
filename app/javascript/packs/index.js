fa9b240ae8d37e4806bad6111ca5e3258f477390
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-14 17:31:36 +0900

    using vue-markdown-highlight

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue/dist/vue.esm';
 import Header from './components/layouts/Header.vue';
 import Router from './router/router';
+import 'highlight.js/styles/default.css'
+import Highlight from 'vue-markdown-highlight';
+
+Vue.use(Highlight);
 
 const app = new Vue({
     el: '#app',
