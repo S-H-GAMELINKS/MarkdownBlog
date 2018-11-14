ff7376da0228293819fb304368d0c8d5a156659b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-14 17:57:17 +0900

    add /blogs/:id delete

--- app/javascript/packs/components/blogs/Index.vue
+++ app/javascript/packs/components/blogs/Index.vue
@@ -3,6 +3,7 @@
         <p v-for="(post, key, index) in posts" :key=index>
             <router-link :to="{name: 'show', params: {id: post.id}}"><h1>{{post.title}}</h1></router-link>
             <router-link :to="{name: 'edit', params: {id: post.id}}">Edit</router-link>
+            <button type="button" class="btn btn-primary" v-on:click="deletePost(post.id)">Delete</button>
         </p>
     </div>
 </template>
@@ -28,6 +29,23 @@ export default {
                 }
             }, (error) => {
                 console.log(error);
+            })
+        },
+        deletePost: function(id) {
+            axios.defaults.headers['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content');
+            axios.defaults.headers['content-type'] = 'application/json';
+
+            axios.delete('/api/posts/' + String(id)).then((response) => {
+
+                for(var i = 0; i < this.posts.length; i++) {
+                    if(this.posts[i].id === id) {
+                        this.posts.slice(1, id);
+                    }
+                }
+
+                alert("Delete it!");
+            }, (error) => {
+                alert(error);
             })
         }
     }
